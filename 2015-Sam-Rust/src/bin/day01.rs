9b8edde8cab86a871db4ae41e352c5c61a353961
Sam Hindess <mail@sohcah.dev>
2021-01-04 14:25:45 +0000

    Begin 2015 AOC in Rust

---
+++ 2015-Sam-Rust/src/bin/day01.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input/01.txt").expect("failure opening input file");
+    let line = input;
+    println!("Part1: {:?}", p1(&line));
+    println!("Part2: {:?}", p2(&line));
+}
+
+fn p1(line: &String) -> i32 {
+  let mut floor = 0i32;
+  for name in line.chars() {
+    if name == '(' {
+      floor = floor + 1;
+    } else {
+      floor = floor - 1;
+    }
+  }
+  return floor;
+}
+
+fn p2(line: &String) -> i32 {
+  let mut floor = 0i32;
+  let mut index = 0;
+  for name in line.chars() {
+    index+=1;
+    if name == '(' {
+      floor = floor + 1;
+    } else {
+      floor = floor - 1;
+    }
+    if floor < 0 {
+      return index;
+    }
+  }
+  return -1;
+}
---
+++ 2015-Sam-Rust/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+  println!(
+      "Usage:\
+      \n - run one day: `cargo run --bin day[01-25]`"
+  );
+}
