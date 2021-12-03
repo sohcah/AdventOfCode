1caf55cb8f1340ac22327dfd75df7608ec658138
Paul Hindess <67744387+BeetrootChessElf@users.noreply.github.com>
2021-12-03 18:21:07 +0000

    Days 1 to 3

---
+++ 2021-Paul/src/days/2.ts
@@ -0,0 +1,34 @@
+
+import fs  from "fs";
+
+export function Part1() {
+
+  const lines = fs.readFileSync("./inputs/2.txt","utf8").trim().replace(/forward/g,'f').replace(/up/g,'u').replace(/down/g,'d').split("\n").map(i=>i.split(" "));
+  let horiz = 0;
+  let depth = 0;
+  for (let i = 0; i<lines.length; i++) {
+    if (lines[i][0] == "f") {horiz+=Number(lines[i][1])};
+    if (lines[i][0] == "d") {depth+=Number(lines[i][1])};
+    if (lines[i][0] == "u") {depth-=Number(lines[i][1])};
+
+  }
+  console.log(horiz,depth,horiz*depth);
+}
+
+export function Part2() {
+
+  const lines = fs.readFileSync("./inputs/2.txt","utf8").trim().replace(/forward/g,'f').replace(/up/g,'u').replace(/down/g,'d').split("\n").map(i=>i.split(" "));
+  let horiz = 0;
+  let depth = 0;
+  let aim = 0;
+  for (let i = 0; i<lines.length; i++) {
+    if (lines[i][0] == "f") {
+      horiz+=Number(lines[i][1])
+      depth+=Number(lines[i][1])*aim
+    };
+    if (lines[i][0] == "d") {aim+=Number(lines[i][1])};
+    if (lines[i][0] == "u") {aim-=Number(lines[i][1])};
+
+  }
+  console.log(horiz,depth,horiz*depth);
+}
