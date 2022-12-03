255ffbcd037f811571fe17c68650e5657e72a411
Sam Hindess <mail@sohcah.dev>
2022-12-03 15:00:02 +0000

    Day 3 Cleanup

--- 2022-Sam/days/3/pt2.ts
+++ 2022-Sam/days/3/pt2.ts
@@ -1,26 +1,26 @@
-import {loadLines, loadNumbers, loadTrimmed, output, sum} from "aocutils";
+import {loadLines, output} from "aocutils";
 
 const lines = loadLines();
 
 // group lines into groups of 3
 const groups = [];
 let group = [];
-for(const line of lines) {
+for (const line of lines) {
   group.push(line);
-  if(group.length === 3) {
+  if (group.length === 3) {
     groups.push(group);
     group = [];
   }
 }
 
 let sum = 0;
-for(const group of groups) {
+for (const group of groups) {
   const bagA = group[0].split("");
   const bagB = new Set(group[1].split(""));
   const bagC = new Set(group[2].split(""));
   const duplicate = bagA.find(i => bagB.has(i) && bagC.has(i));
   let score;
-  if(duplicate.toUpperCase() === duplicate) {
+  if (duplicate.toUpperCase() === duplicate) {
     score = duplicate.charCodeAt(0) - 38;
   } else {
     score = duplicate.charCodeAt(0) - 96;
--- 2022-Sam/days/3/pt2sl.ts
+++ 2022-Sam/days/3/pt2sl.ts
@@ -1,4 +1,30 @@
 import {loadLines, output} from "aocutils";
 import {intersection} from "mnemonist/set";
 
-output(loadLines().reduce((a,b) => a.at(-1).length === 3 ? [...a, [b]] : [...a.slice(0,-1), [...a.at(-1), b]], [[]] as string[][]).reduce((sum,group) => ((cc: number) => sum + cc - (cc >= 97 ? 96 : 38))(Array.from(intersection(new Set(group[0].split("")), new Set(group[1].split("")), new Set(group[2].split(""))))[0].charCodeAt(0)), 0)).forTest(70);
+output(loadLines().reduce((a,b) => a.at(-1).length === 3 ? [...a, [b]] : [...a.slice(0,-1), [...a.at(-1), b]], [[]] as string[][]).reduce((sum,group) => ((cc: number) => sum + cc - (cc >= 97 ? 96 : 38))(Array.from(intersection(new Set(group[0]), new Set(group[1]), new Set(group[2])))[0].charCodeAt(0)), 0)).forTest(70);
+
+/*
+output(
+  loadLines() <-- Load Lines
+    .reduce( <-- Group Lines
+      (a, b) =>
+        a.at(-1).length === 3
+          ? [...a, [b]]
+          : [...a.slice(0, -1), [...a.at(-1), b]],
+      [[]] as string[][]
+    )
+    .reduce(
+      (sum, group) =>
+        ((cc: number) => sum + cc - (cc >= 97 ? 96 : 38))( <-- Calculate Score and Sum
+          Array.from( <-- Detect Duplicate
+            intersection(
+              new Set(group[0]),
+              new Set(group[1]),
+              new Set(group[2])
+            )
+          )[0].charCodeAt(0)
+        ),
+      0
+    )
+).forTest(70);
+ */
