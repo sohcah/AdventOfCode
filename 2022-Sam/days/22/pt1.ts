dd28f796fa8d3b42091b5b902c38e14e57d4b425
Sam Hindess <mail@sohcah.dev>
2022-12-22 07:14:08 +0000

    Day 22 Cleanup

--- 2022-Sam/days/22/pt1.ts
+++ 2022-Sam/days/22/pt1.ts
@@ -1,4 +1,4 @@
-import {IS_TEST, loadInput, loadLines, output} from "aocutils";
+import {loadInput, output} from "aocutils";
 
 const sections = loadInput().trimEnd().split("\n\n");
 const steps = sections[1].match(/\d+|[LR]/g)!.map(i => i.match(/\d+/) ? Number(i) : i as "L" | "R");
@@ -9,7 +9,6 @@ type Pos = [x: number, y: number, facing: 0|1|2|3];
 
 const height = grid.length;
 const width = Math.max(...grid.map(i => i.length));
-console.log(height, width);
 
 function incr(pos: Pos):Pos {
   switch(pos[2]) {
@@ -46,8 +45,6 @@ function moveForward(pos: Pos) {
 
 let pos: Pos = [0, grid[0].indexOf("."), 0];
 
-console.log(pos);
-
 for(const step of steps) {
   if(typeof step === "string") {
     pos = [pos[0], pos[1], (pos[2] + (step === "R" ? 1 : 3)) % 4 as (0|1|2|3)];
@@ -56,9 +53,6 @@ for(const step of steps) {
       pos = moveForward(pos);
     }
   }
-  console.log(step, pos);
 }
 
-// console.log(grid);
-
-output((pos[0]+1) * 1000 + (pos[1]+1) * 4 + pos[2]).forTest(6032);
+output((pos[0]+1) * 1000 + (pos[1]+1) * 4 + pos[2]).forTest(6032).forActual(144244);
