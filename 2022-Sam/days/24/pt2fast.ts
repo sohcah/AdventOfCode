6ce1c513bbca69b97ed5d0daa7c4fb8124f2bf23
Sam Hindess <mail@sohcah.dev>
2022-12-26 14:50:32 +0000

    Day 25 FASTER

--- 2022-Sam/days/24/pt2fast.ts
+++ 2022-Sam/days/24/pt2fast.ts
@@ -4,13 +4,18 @@ const grid = loadLines().map(i => i.split(""));
 
 const height = grid.length - 2;
 const width = grid[0].length - 2;
+const max = Math.max(height, width)
 
 const STORE_FULL_PATH = IS_TEST;
 
+function toVal(coords: [number, number, ...unknown[]]) {
+  return coords[0] * max + coords[1];
+}
+
 const startPos: [number, number] = [grid[0].indexOf(".") - 1, -1];
-const startJoined = startPos.join("|");
+const startVal = toVal(startPos);
 const endPos: [number, number] = [grid.at(-1).indexOf(".") - 1, height];
-const endJoined = endPos.join("|");
+const endVal = toVal(endPos);
 
 let blizzards: [x: number, y: number, direction: number][] = [];
 
@@ -37,56 +42,54 @@ const blizzardDirs: [number, number][] = [
   [width - 1, 0]
 ];
 
-let paths: [number, number][][] = [[startPos]];
+let paths = new Map<number, [number, number][]>();
+paths.set(startVal, [startPos]);
 let answer = -1;
 
 const goals = [endPos, startPos, endPos];
 let goal = 0;
-let goalJoined = goals[goal].join("|");
+let goalVal = toVal(goals[goal]);
 
 for (let round = 0; round < 2000; round++) {
-  // console.log("Round", round);
-
   blizzards = blizzards.map(blizzard => {
     return [(blizzard[0] + blizzardDirs[blizzard[2]][0]) % width, (blizzard[1] + blizzardDirs[blizzard[2]][1]) % height, blizzard[2]];
   });
-  if (IS_TEST) blizzards.sort((a, b) => a[0] - b[0]).sort((a, b) => a[1] - b[1]);
 
   const blizzardSet = new Set();
   for (const blizzard of blizzards) {
-    blizzardSet.add(blizzard.slice(0, 2).join("|"));
+    blizzardSet.add(toVal(blizzard));
   }
 
-  const newPaths = new Map<string, [number, number][]>();
+  const newPaths = new Map<number, [number, number][]>();
 
-  for (const path of paths) {
+  for (const path of paths.values()) {
     const loc = path.at(-1);
     for (const dir of dirs) {
       const newLoc: [number, number] = [loc[0] + dir[0], loc[1] + dir[1]];
-      const joined = newLoc.join("|")
+      const newLocVal = toVal(newLoc);
+      if (newPaths.has(newLocVal)) continue;
+      if (blizzardSet.has(newLocVal)) continue;
       if (newLoc[0] < 0 || newLoc[0] >= width) continue;
-      if ((newLoc[1] < 0 || newLoc[1] >= height) && joined !== startJoined && joined !== endJoined) continue;
-      if (newPaths.has(joined)) continue;
-      if (blizzardSet.has(joined)) continue;
-      if (STORE_FULL_PATH) newPaths.set(joined, [
+      if ((newLoc[1] < 0 || newLoc[1] >= height) && newLocVal !== startVal && newLocVal !== endVal) continue;
+      if (STORE_FULL_PATH) newPaths.set(newLocVal, [
         ...path,
         newLoc
       ]);
-      else newPaths.set(joined, [newLoc])
+      else newPaths.set(newLocVal, [newLoc])
     }
   }
 
-  if (newPaths.has(goalJoined)) {
+  if (newPaths.has(goalVal)) {
     console.log("Found path to goal", goal);
     if (goal === goals.length - 1) {
       answer = round + 1;
       break;
     }
-    paths = [newPaths.get(goalJoined)];
-    goal++;
-    goalJoined = goals[goal].join("|");
+    paths = new Map();
+    paths.set(goalVal, newPaths.get(goalVal));
+    goalVal = toVal(goals[++goal]);
   } else {
-    paths = newPaths.valuesArray();
+    paths = newPaths;
   }
 }
 
